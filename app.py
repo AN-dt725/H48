734ca8d054c3c2f38660b518f692c74dc74d476e
AN-dt725 <anvuhoang15011977@gmail.com>
2025-06-23 01:57:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ df = pd.read_excel("data.xlsx")
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/banhang', methods=['GET', 'POST'])
 def banhang():
